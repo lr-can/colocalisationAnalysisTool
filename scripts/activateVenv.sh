35a3d06b926ae3f975d3f7b6ff59d6322cdb4a77
Lorcan Brenders <lorcan.brenders@etu.univ-lyon1.fr>
2025-02-25 14:34:19 +0100

    auto install hmmer if not present

--- scripts/activateVenv.sh
+++ scripts/activateVenv.sh
@@ -12,6 +12,7 @@ else
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
         bash ~/miniconda.sh -b -p $HOME/miniconda
         export PATH="$HOME/miniconda/bin:$PATH"
+        source ~/.bashrc
         rm ~/miniconda.sh
         echo "Miniconda installed successfully."
     fi
