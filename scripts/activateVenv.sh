7bf2c132ace1fc698a9e4f043c6e60f8ddac3eb8
Lorcan Brenders <lorcan.brenders@etu.univ-lyon1.fr>
2025-02-25 14:25:33 +0100

    chosing conda or venv

--- scripts/activateVenv.sh
+++ scripts/activateVenv.sh
@@ -6,9 +6,16 @@ if [ -d ".venv" ]; then
     echo "Virtual environment has successfully been activated" 
     exit 0
 else
-    echo ".venv directory does not exist. Creating a conda environment..."
-    conda create --prefix ./.venv -y
-    source activate ./.venv
-    echo "Conda environment created and activated."
+    if command -v conda &> /dev/null; then
+        echo ".venv directory does not exist. Creating a conda environment..."
+        conda create --prefix ./.venv -y
+        source activate ./.venv
+        echo "Conda environment created and activated."
+    else
+        echo ".venv directory does not exist. Creating a virtual environment using venv..."
+        python3 -m venv .venv
+        source .venv/bin/activate
+        echo "Virtual environment created and activated using venv."
+    fi
     exit 0
 fi
