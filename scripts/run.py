73334363899e7acd9e48441f5b00d080398d515e
Lorcan Brenders <lorcan.brenders@etu.univ-lyon1.fr>
2025-02-27 11:45:42 +0100

    adding prints

--- scripts/run.py
+++ scripts/run.py
@@ -3,8 +3,32 @@ import subprocess
 import argparse
 import os
 
+class bcolors:
+    """
+    Just a class to store colors for the terminal.
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def parse_arguments():
+    """
+    Parses command-line arguments for the colocalisation analysis tool.
+    Returns:
+        argparse.Namespace: Parsed command-line arguments.
+    Arguments:
+        -f, --file (str): Input file to run colocalisation analysis on.
+        -d, --directory (str): Input directory if you want to run on multiple files.
+        -t, --threads (int): Number of threads to use for geNomad.
+    """
+
     parser = argparse.ArgumentParser(description="Run colocalisation analysis tool")
     parser.add_argument('-f', '--file', type=str, help='Input file to run colocalisation analysis on')
     parser.add_argument('-d', '--directory', type=str, help='Input directory if you want to run on multiple files')
@@ -31,9 +55,20 @@ elif args.file:
         args.file = os.path.realpath(args.file)
     files.append(args.file)
 
+if not args.threads:
+    args.threads = 4
+
+print(f"""{bcolors.OKCYAN} Welcome to the colocalisation analysis tool! 
+This tool has been developed by a group of Master's students from the Claude Bernard Lyon 1 University, under the supervision of COLUZZI, Charles, and PLANTADY, Clarisse.
+{bcolors.ENDC}""")
+print(f"{bcolors.BOLD} Running colocalisation analysis for {len(files)} file{"s" if len(files) > 1 else ""} {bcolors.ENDC}")
 for file_ in files:
+    print(f"{bcolors.OKBLUE} Running colocalisation analysis on {file_} {bcolors.ENDC}")
+    print(f"{bcolors.OKCYAN} Running defenseFinder.sh {bcolors.ENDC}")
     subprocess.check_call(["bash", "./defenseFinder.sh", file_])
+    print(f"{bcolors.OKCYAN} Running geNomad.sh {bcolors.ENDC}")
     subprocess.check_call(["bash", "./geNomad.sh", file_, str(args.threads)])
+    print(f"{bcolors.OKGREEN} Colocalisation analysis finished for {file_} {bcolors.ENDC}")
 
 #genomad = geNomad.install_geNomad()
 #genomad_db = geNomad.download_geNomad_database()
