127bb3501041ff564540e2711dd48278925e4730
magloire aure agbomakounzo <magbomakounzo@pedago-ngs.univ-lyon1.fr>
2025-02-27 01:13:29 +0100

    modified defense finder to run pipeline

--- scripts/defenseFinder.sh
+++ scripts/defenseFinder.sh
@@ -9,4 +9,7 @@ defense-finder update
 
 echo "DefenseFinder is installed and ready to use."
 defense-finder run --help
+
+defense-finder run -o result_Finder ../ressources/GCF_000006765.1.fa.gz
+
 conda deactivate
