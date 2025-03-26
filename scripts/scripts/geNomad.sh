30d9aef4233f82b82fcd78ab781e5b0122743f72
Lorcan Brenders <lorcan.brenders@etu.univ-lyon1.fr>
2025-03-26 17:00:51 +0100

    trying workaroud for genomad database download

--- scripts/scripts/geNomad.sh
+++ scripts/scripts/geNomad.sh
@@ -16,11 +16,22 @@ fi
 conda activate genomad
 
 if [ ! -d "genomad_db" ]; then
-    echo "Directory 'genomad_db' does not exist. Creating directory and downloading database."
-    genomad download-database .
+  echo "Directory 'genomad_db' does not exist. Creating directory and downloading database."
+  if ! genomad download-database .; then
+    echo "Failed to download database using genomad. Attempting to download from Zenodo."
+    mkdir -p genomad_db
+    wget -O genomad_db_v1.7.tar.gz "https://zenodo.org/records/10594875/files/genomad_db_v1.7.tar.gz?download=1"
+    if [ $? -ne 0 ]; then
+      echo "Failed to download database from Zenodo. Exiting."
+      exit 1
+    fi
+    tar -xzf genomad_db_v1.7.tar.gz -C genomad_db --strip-components=1
+    rm genomad_db_v1.7.tar.gz
+  fi
 else
-    echo "Directory 'genomad_db' already exists. Skipping database download."
+  echo "Directory 'genomad_db' already exists. Skipping database download."
 fi
+
 
 echo "Genomad is installed and ready to use."
 
