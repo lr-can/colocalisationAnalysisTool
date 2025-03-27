dcc8eb57d1c9bdee9a398ba71de619a373737b6a
Lorcan Brenders <lorcan.brenders@etu.univ-lyon1.fr>
2025-03-27 15:45:40 +0100

    adding vizualisation

--- scripts/scripts/visualisation.py
+++ scripts/scripts/visualisation.py
@@ -155,7 +155,7 @@ def plot_data(zones_of_interest, tolerance):
         # Get the HTML string for the figure
         html_string = plotly.io.to_html(fig, full_html=True)
         print(f"HTML string for sys_id {sys_id} generated.")
-        result_buffer.append({"plot":html_string, "sys_id":f"{genomad_rows[0]['nom']}_{sys_id}"})
+        result_buffer.append({"plot":html_string, "sys_id":f"{genomad_rows.iloc[0]['nom']}_{sys_id}"})
     return result_buffer
 
 result = plot_data(zones_of_interest, tolerance)
